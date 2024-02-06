113737d7831e9cdf1dd1a33d36134b208b2aa2be
Nishchal Mistry <nm831@cam.ac.uk>
2024-02-06 23:41:12 +0000

    updated

--- Task1B.py
+++ Task1B.py
@@ -1,10 +1,23 @@
-def stations_by_distance(stations, p):
-    
-    
 
 
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_by_distance
+
+def run():
+    '''Build list of stations'''
+    stations = build_station_list()
+    p = (52.2053, 0.1218)
+
+    distance_list_sorted = stations_by_distance(stations, p)
+
+    close = distance_list_sorted[:10]
+    far = distance_list_sorted[-10:]
+    print("10 closest stations to Cambridge city centre are: ", close)
+    print("10 furthest stations to Cambridge city centre are: ", far)
+
+    
 
 
 if __name__ == "__main__":
-    print("*** Task 1A: CUED Part IA Flood Warning System ***")
+    print("*** Task 1B: CUED Part IA Flood Warning System ***")
     run()
