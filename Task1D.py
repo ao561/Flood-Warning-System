44a202a8370dcc7fa548f181899b2628460297b0
Nishchal Mistry <nm831@cam.ac.uk>
2024-02-07 15:46:48 +0000

    Updated

--- Task1D.py
+++ Task1D.py
@@ -4,21 +4,13 @@ from floodsystem.geo import stations_by_river
 def run():
     '''Build list of stations'''
     stations = build_station_list()
-
     river_stations = stations_by_river(stations)
-
     print(len(river_stations),"stations.")
-    
+
     river_stations_keys = (river_stations.keys())
-
     river_stations_keys_sorted = sorted(river_stations_keys)
-
     print("First 10 -", river_stations_keys_sorted[:10])
 
-
-    
-
-    
 
 
 if __name__ == "__main__":
