1e8649f969d89d673f25c1e3d2fc98065fe27c53
Nishchal Mistry <nm831@cam.ac.uk>
2024-02-07 15:13:55 +0000

    Enhanced

--- Task1D.py
+++ Task1D.py
@@ -1,17 +1,18 @@
 from floodsystem.stationdata import build_station_list
-from floodsystem.geo import stations_by_distance
+from floodsystem.geo import stations_by_river
 
 def run():
     '''Build list of stations'''
     stations = build_station_list()
-    p = (52.2053, 0.1218)
 
-    distance_list_sorted = stations_by_distance(stations, p)
+    river_stations = stations_by_river(stations)
 
-    close = distance_list_sorted[:10]
-    far = distance_list_sorted[-10:]
-    print("10 closest stations to Cambridge city centre are: {}" .format(close))
-    print("10 furthest stations to Cambridge city centre are: {}" .format(far))
+    print(len(river_stations),"stations.")
+    river_stations_sorted = river_stations.sort()
+
+    print("First 10 -", river_stations_sorted[:10])
+    
+    
 
     
 
