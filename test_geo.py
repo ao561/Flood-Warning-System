6a83fb8e4d0d2855455830f19b0514a9bbd3f29f
Nishchal Mistry <nm831@cam.ac.uk>
2024-02-07 20:16:22 +0000

    Added test 1D

--- test_geo.py
+++ test_geo.py
@@ -21,6 +21,19 @@ from floodsystem.station import inconsistent_typical_range_stations
 
 '''Task 1D'''
 
+def test_rivers_with_station():
+    '''test for number of stations and making sure each river has at least one station'''
+    stations = build_station_list()
+    rivers_stations = rivers_with_station(stations)
+    assert len(r) == 1012
+    for a in stations:
+        for b in rivers_stations:
+            n = 0
+            if a.river == j:
+                n = n + 1
+                assert n >= 1
+
+
 
 
 '''Task 1E'''
