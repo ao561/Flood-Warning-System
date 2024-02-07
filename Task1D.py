458c4005f4bbdb50fa4807585c402242849f9115
Nishchal Mistry <nm831@cam.ac.uk>
2024-02-07 16:01:11 +0000

    updated

--- Task1D.py
+++ Task1D.py
@@ -1,18 +1,23 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_by_river
+from floodsystem.geo import rivers_with_station
+
 
 def run():
     '''Build list of stations'''
     stations = build_station_list()
-    river_stations = stations_by_river(stations)
-    print(len(river_stations),"stations.")
+    rivers = rivers_with_station(stations)
+    print(len(rivers),"stations.")
 
-    river_stations_keys = (river_stations.keys())
-    river_stations_keys_sorted = sorted(river_stations_keys)
-    print("First 10 -", river_stations_keys_sorted[:10])
+    rivers.sort()
+    
+    print("First 10 -", rivers[:10])
+
+
 
 
 
 if __name__ == "__main__":
     print("*** Task 1D: CUED Part IA Flood Warning System ***")
     run()
+
