6d9f68a17baa4847c6d3b647b4e2ea695dfe1643
Nishchal Mistry <nm831@cam.ac.uk>
2024-02-07 19:49:35 +0000

    Added imports

--- test_geo.py
+++ test_geo.py
@@ -1,5 +1,42 @@
-import mymodule
+from .utils import sorted_by_key  # noqa
+import numpy
+from . import datafetcher
+from .stationdata import build_station_list
+from .station import MonitoringStation
+import math
+from floodsystem.geo import rivers_with_station
+from floodsystem.geo import stations_by_river
+from floodsystem.geo import stations_within_radius
+from floodsystem.station import inconsistent_typical_range_stations
 
-def test_my_sum():
-    sum = mymodule.sum(7, -8)
-    assert sum == -1
+
+
+
+
+
+'''Task 1B'''
+
+
+
+'''Task 1C'''
+
+
+
+'''Task 1D'''
+
+def test_rivers_with_station():
+    stations = build_station_list()
+    r = rivers_with_station(stations)
+    assert len(r) == 857
+    for i in stations:
+        for j in r:
+            a = 0
+            if i.river == j:
+                a = a+1
+                assert a>= 1
+
+'''Task 1E'''
+
+
+
+'''Task 1F'''
