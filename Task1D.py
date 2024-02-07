e23af94e82fb7f221a82e0387a24d48ed767897f
Nishchal Mistry <nm831@cam.ac.uk>
2024-02-07 15:42:28 +0000

    Added key functionality

--- Task1D.py
+++ Task1D.py
@@ -9,11 +9,12 @@ def run():
 
     print(len(river_stations),"stations.")
     
-    river_stations_sorted = sorted(river_stations.items())
+    river_stations_keys = (river_stations.keys())
 
-    river_stations_sorted_keys = river_stations_sorted.keys()
+    river_stations_keys_sorted = sorted(river_stations_keys)
 
-    print("First 10 -", river_stations_sorted_keys[:10])
+    print("First 10 -", river_stations_keys_sorted[:10])
+
 
     
 
