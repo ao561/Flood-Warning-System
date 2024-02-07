0ebd998d63f9f3fc2f8803bb1d8ed6716ef80662
ao561 <ao561@cam.ac.uk>
2024-02-07 19:18:10 +0000

    1c

---
+++ Task1C.py
@@ -0,0 +1,15 @@
+
+
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_within_radius
+
+def run():
+    stations = build_station_list()
+    cam_city_centre_coordinate = (52.2053, 0.1218)
+    stations_within_radius_sorted = sorted(stations_within_radius(stations, cam_city_centre_coordinate, 10))
+    print(stations_within_radius_sorted)
+
+if __name__ == "__main__":
+    print("*** Task 1A: CUED Part IA Flood Warning System ***")
+    run()
+
