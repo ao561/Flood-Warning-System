1f40a202eb21c129ce8a29951e6e89bfd6d18563
Nishchal Mistry <nm831@cam.ac.uk>
2024-02-26 16:00:29 +0000

    updated

--- Task2B.py
+++ Task2B.py
@@ -2,14 +2,15 @@ from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_level_over_threshold
 
 def run():
-    # Build list of stations
+    print("hello")
+    '''# Build list of stations
     stations = build_station_list()
 
     # Update latest level data for all stations
     update_water_levels(stations)
 
     stations_level_over_threshold(stations, 0.8)
-
+    '''
 
     '''for station in stations:
         if station.name in names:
