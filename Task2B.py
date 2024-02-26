0be6b817a5f96ea91f4194e3110250ed1b13db52
Nishchal Mistry <nm831@cam.ac.uk>
2024-02-26 15:55:10 +0000

    updated

--- Task2B.py
+++ Task2B.py
@@ -1,5 +1,5 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
-
+from floodsystem.flood import stations_level_over_threshold
 
 def run():
     # Build list of stations
@@ -8,22 +8,15 @@ def run():
     # Update latest level data for all stations
     update_water_levels(stations)
 
-    # Print station and latest level for first 5 stations in list
-    names = [
-        'Bourton Dickler', 'Surfleet Sluice', 'Gaw Bridge', 'Hemingford',
-        'Swindon'
-    ]
-    for station in stations:
+    stations_level_over_threshold(stations, 0.8)
+
+
+    '''for station in stations:
         if station.name in names:
             print("Station name and current level: {}, {}".format(
-                station.name, station.latest_level))
-
-    # Alternative implementation
-    # for station in [s for s in stations if s.name in names]:
-    #     print("Station name and current level: {}, {}".format(station.name,
-    #                                                           station.latest_level))
+                station.name, station.latest_level))'''
 
 
 if __name__ == "__main__":
-    print("*** Task 2A: CUED Part IA Flood Warning System ***")
+    print("*** Task 2B: CUED Part IA Flood Warning System ***")
     run()
